4f242ae4cfbef05cff0261bd17af01b152628f90
shivkumarsaini <shivam.16490@gmail.com>
2021-09-05 09:58:04 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
+import { useEffect, useCallback, useState } from 'react';
+import { Drink, DrinkList } from './drink.js';
+import data from './data.js';
 import './App.css';
 
 function App() {
+  let [myDrinks, setMyDrinks] = useState(data);
+
+  // useEffect(() => {
+
+  // }, []);
+
+  const getDrinks = useCallback(async () => {
+    let resp = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+      .then(response => response.json())
+      .then(res => {
+        setMyDrinks(res.drinks);
+        debugger;
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className=""> */}
+        <div className="App-header container-fluid">
+          
+          <DrinkList myDrinks={myDrinks} drinkLoader={getDrinks}></DrinkList>
+        </div>
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +39,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
     </div>
   );
 }
